c253c3218c221ea3023e40c66289ced11ffd0819
chetanrathor <chetanrathore3899@gmail.com>
2023-04-11 08:37:43 +0530

    created commonmodule

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TabbarComponent } from './components/tabbar/tabbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    TabbarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
